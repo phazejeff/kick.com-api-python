5144fb28c3c25e8383de77b4c16afe467aab5c22
phazejeff <85849384+phazejeff@users.noreply.github.com>
2023-06-23 16:38:39 -0700

    Remove unused code

--- kickAPI/auth.py
+++ kickAPI/auth.py
@@ -20,40 +20,6 @@ class TokenProvider:
             "password": password
         }
 
-# class Auth:
-#     def __init__(self, auth_param_name, auth_token, kick_session, xsrf_token):
-#         self.auth_param_name = auth_param_name
-#         self.auth_token = auth_token
-#         self.kick_session = kick_session
-#         self.xsrf_token = xsrf_token
-    
-#     def get_cookies(self):
-#         return {
-#             "kick_session" : self.kick_session,
-#             "XSRF-TOKEN": self.xsrf_token,
-#             self.auth_param_name : self.auth_token
-#         }
-    
-#     def set_bearer(self, bearer):
-#         self.bearer = "Bearer " + bearer
-
-# def get_session():
-#     r = scraper.get(KICK_BASE_URL, headers={"content-type": "text/html; charset=UTF-8"})
-#     cookies: List[Tuple(str, str)] = r.cookies.items()
-#     for c in r.cookies.items():
-#         if c[0] == "__cf_bm":
-#             cookies.remove(c)
-#         elif c[0] == "kick_session":
-#             kick_session = c[1].replace("%3D", "=")
-#             cookies.remove(c)
-#         elif c[0] == "XSRF-TOKEN":
-#             xsrf_token = c[1].replace("%3D", "=")
-#             cookies.remove(c)
-#     auth_param_name = cookies[0][0]
-#     auth_token = cookies[0][1].replace("%3D", "=") # by process of elimination, the last thing must be the auth token
-
-#     return Auth(auth_param_name, auth_token, kick_session, xsrf_token)
-
 class Auth:
     def __init__(self, scraper):
         self.scraper = scraper
